132a2ed2c002c6ddd4737fb65e3a0c7b847abf77
Michał Ochman <michoch@gmail.com>
2018-05-08 13:57:47 +0200

    Fix alias tests

--- test/react-pixi-alias/index.test.js
+++ test/react-pixi-alias/index.test.js
@@ -1,22 +1,22 @@
 import * as ReactPIXI from "../../src/react-pixi-alias/index";
-import * as ReactPixi from "../../src/index";
+import * as ReactPixiFiber from "react-pixi-fiber";
 import ReactDOM from "react-dom";
 
 describe("react-pixi-alias", () => {
   it("provides expected methods", () => {
-    expect(ReactPIXI.CustomPIXIComponent).toEqual(ReactPixi.CustomPIXIComponent);
+    expect(ReactPIXI.CustomPIXIComponent).toEqual(ReactPixiFiber.CustomPIXIComponent);
     expect(ReactPIXI.render).toEqual(ReactDOM.render);
     expect(ReactPIXI.unmountComponentAtNode).toEqual(ReactDOM.unmountComponentAtNode);
   });
 
   it("provides expected components", () => {
-    expect(ReactPIXI.BitmapText).toEqual(ReactPixi.BitmapText);
-    expect(ReactPIXI.DisplayObjectContainer).toEqual(ReactPixi.Container);
-    expect(ReactPIXI.Graphics).toEqual(ReactPixi.Graphics);
-    expect(ReactPIXI.ParticleContainer).toEqual(ReactPixi.ParticleContainer);
-    expect(ReactPIXI.Sprite).toEqual(ReactPixi.Sprite);
-    expect(ReactPIXI.Stage).toEqual(ReactPixi.Stage);
-    expect(ReactPIXI.Text).toEqual(ReactPixi.Text);
-    expect(ReactPIXI.TilingSprite).toEqual(ReactPixi.TilingSprite);
+    expect(ReactPIXI.BitmapText).toEqual(ReactPixiFiber.BitmapText);
+    expect(ReactPIXI.DisplayObjectContainer).toEqual(ReactPixiFiber.Container);
+    expect(ReactPIXI.Graphics).toEqual(ReactPixiFiber.Graphics);
+    expect(ReactPIXI.ParticleContainer).toEqual(ReactPixiFiber.ParticleContainer);
+    expect(ReactPIXI.Sprite).toEqual(ReactPixiFiber.Sprite);
+    expect(ReactPIXI.Stage).toEqual(ReactPixiFiber.Stage);
+    expect(ReactPIXI.Text).toEqual(ReactPixiFiber.Text);
+    expect(ReactPIXI.TilingSprite).toEqual(ReactPixiFiber.TilingSprite);
   });
 });
