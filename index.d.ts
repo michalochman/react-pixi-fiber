0a6001e98093a57be140767f9797a99637537165
Hibikine Kage <goodsmile314159@gmail.com>
2018-05-13 06:27:50 +0900

    Add custom component definisions

--- index.d.ts
+++ index.d.ts
@@ -125,4 +125,35 @@ declare module 'react-pixi-fiber' {
   /** Custom React Reconciler render method. */
   export function render(pixiElement: PIXI.DisplayObject | PIXI.DisplayObject[], stage: PIXI.Container, callback?: Function): void;
 
+  /**
+   * Custom component properties.
+   */
+  export interface Behavior<T, U extends PIXI.DisplayObject> {
+    /**
+     * Use this to create an instance of [PIXI.DisplayObject].
+     */
+    customDisplayObject: (props: T) => U;
+    /**
+     * Use this to apply newProps to your Component in a custom way.
+     */
+    customApplyProps?: (displayObject: U, oldProps: T, newProps: T) => any;
+    /**
+     * Use this to do something after displayObject is attached, which happens after componentDidMount lifecycle method.
+     */
+    customDidAttach?: (displayObject: U) => any;
+    /**
+     * Use this to do something (usually cleanup) before detaching, which happens before componentWillUnmount lifecycle method.
+     */
+    customWillDetach?: (displayObject: U) => any;
+  }
+  /**
+   * Create a custom component.
+   */
+  export function CustomPIXIComponent<T, U extends PIXI.DisplayObject>(
+    behavior: Behavior<T, U>,
+    /**
+     * The name of this custom component.
+     */
+    type: string
+  ): React.Component<T>;
 }
