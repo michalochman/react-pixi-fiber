a9fcf3f2009b8651b2892bce9708452fb0223df7
mushus <mushus.wynd@gmail.com>
2018-06-11 01:35:48 +0900

    Fix memory leak when mounting/unmounting Stage (#61)

--- src/Stage.js
+++ src/Stage.js
@@ -115,6 +115,7 @@ class Stage extends React.Component {
 
   componentWillUnmount() {
     ReactPixiFiber.updateContainer(null, this._mountNode, this);
+    this._app.destroy();
   }
 
   render() {
