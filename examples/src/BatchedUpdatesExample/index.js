f718bdbd40a24c78900711c2840c433ac9308a2d
Michał Ochman <michoch@gmail.com>
2018-12-26 19:54:42 +0100

    Update unstable_batchedUpdates example

--- examples/src/BatchedUpdatesExample/index.js
+++ examples/src/BatchedUpdatesExample/index.js
@@ -1,70 +1,71 @@
-import React, { Component } from "react";
-import { render, unstable_batchedUpdates } from "./react-pixi-fiber.development";
+import React, { Component, Fragment } from "react";
+import { render, unstable_batchedUpdates } from "react-pixi-fiber";
 import Bunny from "../Bunny";
-import * as PIXI from 'pixi.js';
+import * as PIXI from "pixi.js";
 
 const OPTIONS = {
   backgroundColor: 0x1099bb,
 };
-const asyncFunc = (cb) => {
+const asyncFunc = cb => {
   setTimeout(cb, 100);
-}
+};
 
 class Canvas extends Component {
-  mountStage = (canvasEl) => {
+  mountStage = canvasEl => {
     this.app = new PIXI.Application({
       ...OPTIONS,
-      view: canvasEl
+      view: canvasEl,
     });
-    render(<BatchedUpdatesExample/>, this.app.stage)
-  }
+    render(<BatchedUpdatesExample />, this.app.stage);
+  };
 
   render() {
-    return (
-      <canvas ref={this.mountStage} width="600" height="800"></canvas>
-    )
+    return <canvas ref={this.mountStage} width="600" height="800" />;
   }
 }
 
 class BatchedUpdatesExample extends Component {
   state = {
     x: 0,
-    y: 0
-  }
+    y: 0,
+  };
 
   componentDidMount() {
-    // auto batched in life circle
+    // auto batched in life cycle
     // render once
     this.setStateTwice();
-
-    // render once
-    asyncFunc(
-      unstable_batchedUpdates(
-        this.setStateTwice
-      )
-    )
-
-    // render twice
-    asyncFunc(this.setStateTwice)
   }
 
   setStateTwice = () => {
     this.setState({
       x: 100,
       y: 200,
-    })
+    });
     this.setState({
       x: 300,
       y: 400,
-    })
-  }
+    });
+  };
+
+  batched = () => {
+    // render once
+    asyncFunc(unstable_batchedUpdates(this.setStateTwice));
+  };
+
+  normal = () => {
+    // render twice
+    asyncFunc(this.setStateTwice);
+  };
 
   render() {
+    console.log("`render` called");
+    const width = 800;
+    const height = 600;
     return (
-      <Bunny
-        x={this.state.x}
-        y={this.state.y}
-      />
+      <Fragment>
+        <Bunny x={this.state.x} y={this.state.y} interactive pointerdown={this.normal} />
+        <Bunny x={width - this.state.x} y={height - this.state.y} interactive pointerdown={this.batched} />
+      </Fragment>
     );
   }
 }
