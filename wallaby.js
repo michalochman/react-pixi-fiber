94ce1f3bb3d4bd16aa7ec77dac4273f3054dcfdd
Michał Ochman <michoch@gmail.com>
2019-02-23 22:58:12 +0100

    Update Wallaby config

--- wallaby.js
+++ wallaby.js
@@ -1,17 +1,14 @@
 module.exports = function(wallaby) {
   return {
-    files: ["src/**/*.js", "package.json"],
+    files: ["src/**/*.js", "package.json", "./config/jest/**/*.js"],
     tests: ["test/**/*.js"],
     compilers: {
       "**/*.js": wallaby.compilers.babel(),
     },
     env: {
       type: "node",
-      runner: "node",
     },
     // https://wallabyjs.com/docs/integration/jest.html
     testFramework: "jest",
-    // https://medium.com/@artem.govorov/jest-snapshot-testing-on-steroids-with-wallaby-js-a53008f619f0
-    // https://github.com/wallabyjs/public/issues/870
   };
 };
