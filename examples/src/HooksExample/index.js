4b6321f007e91da13a7a1fce613e7a424c430a48
nils <nils@gangshi>
2019-09-16 01:15:01 -0400

    added more hooks to the hooks example

--- examples/src/HooksExample/index.js
+++ examples/src/HooksExample/index.js
@@ -1,4 +1,12 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, {
+  Fragment,
+  useCallback,
+  useContext,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { Stage, Text, usePixiTicker } from "react-pixi-fiber";
 import Circle from "../CustomPIXIComponentExample/Circle";
 import Rect from "../CustomPIXIComponentExample/Rect";
@@ -10,6 +18,10 @@ const grayFromNum = num => {
   return parseInt(`${hex.repeat(3)}`, 16);
 };
 
+/**
+ * Implements `react-pixi-fiber`'s `usePixiTicker` hook, and the `useState` hook.
+ * Handles animation of the circle and square background.
+ */
 function useAnimatedValue({ direction, max, min, value }) {
   const [data, setData] = useState({
     direction,
@@ -23,9 +35,12 @@ function useAnimatedValue({ direction, max, min, value }) {
     setData(current => {
       const data = { ...current };
 
+      // flip direction once min or max has been reached.
       if ((current.value >= max && current.direction === 1) || (current.value <= min && current.direction === -1)) {
         data.direction *= -1;
       }
+
+      // increment or decrement `
       data.value += data.direction;
 
       return data;
@@ -35,12 +50,30 @@ function useAnimatedValue({ direction, max, min, value }) {
   return data.value;
 }
 
+const AnimationContext = React.createContext();
+
+/**
+ * implements `useContext`, `useLayoutEffect` and `useRef`.
+ */
+const Title = () => {
+  const { title } = useContext(AnimationContext);
+  const pixiText = useRef(null);
+
+  // horizontally center the title's pivot point. this also works fine with `useEffect`.
+  useLayoutEffect(() => {
+    pixiText.current.pivot.set(pixiText.current.width / 2, 0);
+  }, [title]);
+
+  return <Text ref={pixiText} text={title} x={400} y={0} />;
+};
+
 const Animation = () => {
   const number1 = useAnimatedValue({ direction: 1, max: 255, min: 0, value: 0 });
   const number2 = useAnimatedValue({ direction: -1, max: 255, min: 0, value: 255 });
   const [text1, setText1] = useState(`number1: ${number1}`);
   const [text2, setText2] = useState(`number2: ${number2}`);
 
+  // update the contents of the `Text` instances.
   useEffect(() => {
     setText1(`number1: ${number1}`);
     setText2(`number2: ${number2}`);
@@ -48,6 +81,7 @@ const Animation = () => {
 
   return (
     <Fragment>
+      <Title />
       <Rect x={275} y={175} width={250} height={250} fill={grayFromNum(number1)} />
       <Circle x={400} y={300} radius={100} fill={grayFromNum(number2)} />
       <Text text={text1} x={0} y={500} />
@@ -58,8 +92,10 @@ const Animation = () => {
 
 const HooksExample = () => {
   return (
-    <Stage width={800} height={600} options={{ backgroundColor: 0xff0000 }}>
-      <Animation />
+    <Stage width={800} height={600} options={{ backgroundColor: 0xff0000}}>
+      <AnimationContext.Provider value={{ title: 'animation' }}>
+        <Animation />
+      </AnimationContext.Provider>
     </Stage>
   );
 };
