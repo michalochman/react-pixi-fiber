e0e92f57d10cee63f4cd3db9e3b98f79f4926054
Michał Ochman <michoch@gmail.com>
2019-11-23 18:00:44 +0100

    Fix compatibility with React older than 16.3.0

--- src/compat.js
+++ src/compat.js
@@ -4,6 +4,14 @@ export function areReactHooksAvailable() {
   return typeof React.useEffect === "function";
 }
 
+export function createRef() {
+  return typeof React.createRef === "function"
+    ? React.createRef()
+    : {
+        current: null,
+      };
+}
+
 export function isNewContextAvailable() {
   return typeof React.createContext === "function";
 }
