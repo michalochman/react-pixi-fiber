a52908b4c92015f2561dba3507fe42bb71259011
Michał Ochman <michoch@gmail.com>
2019-11-23 19:03:46 +0100

    Set default props based on type

--- src/PixiPropertyOperations.js
+++ src/PixiPropertyOperations.js
@@ -1,7 +1,14 @@
 // Based on: https://github.com/facebook/react/blob/27535e7bfcb63e8a4d65f273311e380b4ca12eff/packages/react-dom/src/client/DOMPropertyOperations.js
 import { getPropertyInfo, shouldIgnoreAttribute, shouldRemoveAttribute } from "./PixiProperty";
-import { DEFAULT_PROPS } from "./props";
+import { defaultProps } from "./props";
 import { setPixiValue } from "./utils";
+
+export function getDefaultValue(type, propName) {
+  const defaultValues = defaultProps[type];
+  if (typeof defaultValues !== "undefined") {
+    return defaultValues[propName];
+  }
+}
 
 /**
  * Sets the value for a property on a PIXI.DisplayObject instance.
@@ -19,7 +26,7 @@ export function setValueForProperty(type, instance, propName, value) {
   }
   if (shouldRemoveAttribute(type, propName, value, propertyInfo)) {
     // Try to reset to property to default value (if it is defined)
-    const defaultValue = DEFAULT_PROPS[propName];
+    const defaultValue = getDefaultValue(type, propName);
     if (typeof defaultValue !== "undefined") {
       value = defaultValue;
     } else {
